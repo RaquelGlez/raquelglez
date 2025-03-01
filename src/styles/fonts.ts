2c7cc47df485a5096bae8611076279edb51b66b5
Raquel Gonzalez <gonzalez.c.raquel@gmail.com>
2025-03-01 13:27:26 -0600

    initial configuration is added

---
+++ src/styles/fonts.ts
@@ -0,0 +1,7 @@
+import { Playfair_Display, Raleway, Lato } from "next/font/google";
+
+export const playfairDisplay =  Playfair_Display({subsets:['latin'], weight:['400', '500']});
+
+export const raleway =  Raleway({subsets:['latin'], weight:['300', '400', '500']});
+
+export const lato =  Lato({subsets:['latin'], weight:['100', '300']})
